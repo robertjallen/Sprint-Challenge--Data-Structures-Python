f51fbf3a686da69d5b3495b12a33b20c12f44784
robertjallen <bobbidigi34@gmail.com>
2020-08-30 19:15:40 -0400

    add reverse_list MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -40,23 +40,23 @@ class LinkedList:
 
     def reverse_list(self, node, prev):
         # create a previous node and set value to None
-
+        previous_node = None
         # create a variable to store the current node
         # set it to the current head
-
+        current_node = self.head
         # create a while loop that runs while the list
         # is not empty or the value of None
-
-
-        # set the next node to the currents next 
-        # which is technically the head's next node
-
-        # reset the value of the current next node to
-        # the previous node
-        
-        # the previous node becomes the next node 
-        
-        # the current node then becomes the next node
-
+        while current_node is not None:
+            # set the next node to the currents next 
+            # which is technically the head's next node
+            next_node = current_node.next_node
+            # reset the value of the current next node to
+            # the previous node
+            current_node.next_node = previous_node
+            # the previous node becomes the next node 
+            previous_node = current_node
+            # the current node then becomes the next node
+            current_node = next_node
         # the new head becomes the previous node
         # thus reversing the order of the linked list
+        self.head = previous_node
